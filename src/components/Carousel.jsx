b104671e8d107d84610e8ebf547d5c6ce9d0bcad
suman492 <suman.pat49@gmail.com>
2025-12-23 22:25:41 +0530

    Integrated Ref to the Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -6,19 +6,27 @@ import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
 
   const handleNext = () =>
     setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
-  const handlePrevious = () =>
+  const handlePrevious = () => {
     setCurrentIndex(
       prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
     );
+    resetTimer();
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
+  };
 
   useEffect(() => {
-    const interval = setInterval(handleNext, 3000);
+    timerRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timerRef.current);
   }, []);
 
   return (
@@ -38,7 +46,10 @@ const Carousel = ({ imageUrls, title }) => {
         className="shrink-0 focus-within:ring-0 hover:bg-transparent"
         icon={Right}
         style="text"
-        onClick={handleNext}
+        onClick={() => {
+          handleNext();
+          resetTimer();
+        }}
       />
       <div className="flex space-x-1">
         {imageUrls.map((_, index) => (
@@ -48,7 +59,10 @@ const Carousel = ({ imageUrls, title }) => {
               "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
               { "neeto-ui-bg-black": index === currentIndex }
             )}
-            onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
           />
         ))}
       </div>
